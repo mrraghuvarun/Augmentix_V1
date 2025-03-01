70afcfc8038b8b3e5b1a65a1cb252a6c5bc0a948
Nunna Charan Naga Lakshmi Narayana <109160426+nunnacharan@users.noreply.github.com>
2025-03-01 13:50:38 +0530

    Update vite.config.js

--- Augmentix_V0/vite.config.js
+++ Augmentix_V0/vite.config.js
@@ -1,26 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    //port: 5173,
-    // Only keep this proxy section if you decide to use relative URLs in your API calls
-    // Otherwise, remove it if you're using direct API_BASE_URL
-    proxy: {
-      '/api/': {
-        target: 'https://1pdafh6hmf.execute-api.us-west-2.amazonaws.com/dev',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        compact: false
-      }
-    }
+  plugins: [react()],
+  define: {
+    'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
   }
-})
+});
